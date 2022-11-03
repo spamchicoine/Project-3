b0ce6f3d6ecc0daeb82c166002cad5df6c2d033d
spamchicoine <114253734+spamchicoine@users.noreply.github.com>
2022-11-03 17:23:15 -0400

    App1 and App2

--- MovieBST.py
+++ MovieBST.py
@@ -1,1 +1,69 @@
-### To complete
+from Movie import *
+
+class Node():
+
+    def __init__(self,movie):
+        self.movie = movie
+        self.left = None
+        self.right = None
+
+    def __str__(self): # Overloads str() so Node returns the movie data
+        return str(self.movie)
+
+class MovieBST():
+    
+    def __init__(self,file):
+        self.root = None
+        self.__size = 0
+        nodes = open(file)
+        for n in nodes: #Inserts the data from each line of the text file as a movie object node
+            data = n.split('\n')[0].split(';') # Gets useful data from each text line
+            self.insert(Movie(int(data[0]),int(data[1]),data[2])) # Inserts movie object as a node
+    
+    def getSize(self):
+        return self.__size
+
+    def isEmpty(self):
+        return self.root is None
+    
+    def insert(self,movie): 
+        newNode = Node(movie)
+        if self.__size == 0: #Corner case if BST is empty
+            self.root = newNode
+            self.__size += 1
+        else:
+            self.recInsert(self.root,newNode) # Begins recursive insertion
+
+    def recInsert(self,current,new): # Recursively compares data to insert with left and right nodes
+        if new.movie.getID()<current.movie.getID(): # Search left
+            if current.left is None:
+                current.left=new # Insert node
+                self.__size+=1
+            else:
+                self.recInsert(current.left,new)
+        elif new.movie.getID()>=current.movie.getID(): # Search right
+            if current.right is None:
+                current.right=new # Insert node
+                self.__size+=1
+            else:
+                self.recInsert(current.right,new)
+
+    def search(self,id): # Searches BST for given ID
+        if self.isEmpty(): # Corner case if BST is empty
+            return None
+        else:
+            node=self.recSearch(self.root,id) # Starts recursive searching
+        return node
+    
+    def recSearch(self,current,id):
+        if current is None: return None # ID not found and reached end of BST
+        if id<current.movie.getID(): # Searches left subtree if ID is smaller than current
+            temp=self.recSearch(current.left,id)
+        elif id>current.movie.getID(): # Searches right subtree if ID is greater than current
+            temp=self.recSearch(current.right,id)
+        else:
+            temp=current # ID found
+        return temp
+
+
+moviesbst = MovieBST('Movies.txt')
--- MovieList.py
+++ MovieList.py
@@ -3,3 +3,41 @@ import random
 import sys
 
 class MovieList:
+    def __init__(self,file):
+        text = open(file) # Opens file in read mode
+        self.__list =[]
+        for n in text: # Appands a movie object using the data from the text file to our list
+            data = n.split('\n')[0].split(';') # Splits each text line in useful data
+            self.__list.append(Movie(data[0],data[1],data[2]))
+    
+    def getSize(self):
+        return len(self.__list)
+    
+    def binarySearch(self,id): # Binary search algorithm that finds the inputted id
+        lower = 0
+        upper = len(self.__list)-1
+        while lower <= upper:
+            middle = lower + (upper - lower)//2 # Updates middle index
+            if int(self.__list[middle].getID()) == id:   # Checks if middle index matches id searched for
+                self.__steps += 1
+                return self.__list[middle]
+            
+            elif int(self.__list[middle].getID()) < id: # Adjusts lower bound if item at middle index is less than id searched for
+                lower = middle + 1
+                self.__steps += 1
+            
+            elif int(self.__list[middle].getID()) > id: # Adjusts upper bound if item at middle index is greater than id searched for
+                upper = middle - 1
+                self.__steps += 1
+
+        return 'Movies not found'
+    
+    def shuffle(self): # Shuffles inputted list
+        for i in range(len(self.__list)-1,0,-1):
+            index = random.randint(0,i) # Creates a random index
+            self.__list[i],self.__list[index] = self.__list[index],self.__list[i] # Swap values using random index
+
+    def save(self,filename): # Saves list data to a new file named by input
+        write = open(filename,'w') # Opens file of filename in write mode
+        for i in self.__list:
+            write.write(str(i)+'\n') # Writes each list item on each line of the text line
